0938c3740a541382bb0d7feb66a7058152c7aca7
Francesco Michelini <hello@francescomichelini.com>
2023-11-26 19:34:53 +0100

    Add movement in script

--- godot_logo.gd
+++ godot_logo.gd
@@ -4,7 +4,22 @@ var speed = 200
 var angular_speed = PI
 
 func _process(delta):
-	rotation += angular_speed * delta
+	var direction = 0
 	
-	var velocity = Vector2.UP.rotated(rotation) * speed
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+		
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+		
+	rotation += angular_speed * delta * direction
+	
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed('ui_up'):
+		velocity = Vector2.UP.rotated(rotation) * speed
+		
+	if Input.is_action_pressed('ui_down'):
+		velocity = Vector2.DOWN.rotated(rotation) * speed
+		
 	position += velocity * delta
