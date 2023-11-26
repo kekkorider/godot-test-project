33b85fa59f6c71a6065b2a1386f29d61d19a93a4
Francesco Michelini <hello@francescomichelini.com>
2023-11-26 20:35:46 +0100

    Signals

--- godot_logo.gd
+++ godot_logo.gd
@@ -1,7 +1,17 @@
 extends Sprite2D
 
+signal custom_toggle
+
 var speed = 200
 var angular_speed = PI
+var click_count = 0
+
+func _ready():
+	if has_node_and_resource('Timer'):
+		var timer = get_node("Timer")
+		timer.timeout.connect(_on_timer_timeout)
+		
+	custom_toggle.connect(_on_custom_toggle)
 
 func _process(delta):
 	var direction = 0
@@ -23,3 +33,16 @@ func _process(delta):
 		velocity = Vector2.DOWN.rotated(rotation) * speed
 		
 	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing())
+	custom_toggle.emit(click_count)
+
+func _on_timer_timeout():
+	visible = not visible
+
+func _on_custom_toggle(count):
+	print("_on_custom_toggle()")
+	click_count += 1
+	print(count, '->', click_count)
